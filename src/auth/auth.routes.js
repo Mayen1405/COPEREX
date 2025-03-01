4833002d460c22014889f3a3d27f7d561d47588a
Mayen1405 <melgar14jose@gmail.com>
2025-02-28 23:54:13 -0600

    add model user , middlewares

---
+++ src/auth/auth.routes.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import { register, login } from "./auth.controller.js";
+import { registerValidator, loginValidator } from "../middlewares/admin-validator.js";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /coperex/v1/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post("/register", registerValidator, register);
+
+/**
+ * @swagger
+ * /coperex/v1/auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post("/login", loginValidator, login);
+
+export default router;
